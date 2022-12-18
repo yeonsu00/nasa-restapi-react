7211b3400758fc292e338b70e0fe528614ca0e83
kim-yeonsuu <yeonsu0329@kyonggi.ac.kr>
2022-12-19 00:30:40 +0900

    [feat] 컴포넌트 나눔 (Search, List)

---
+++ src/component/List.js
@@ -0,0 +1,14 @@
+import logo from '../logo.svg';
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { HashRouter, Route } from 'react-router-dom';
+
+function List() {
+  return (
+    <div>
+        영화 리스트
+    </div>
+  );
+}
+
+export default List;
---
+++ src/component/Search.js
@@ -0,0 +1,14 @@
+import logo from '../logo.svg';
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { HashRouter, Route } from 'react-router-dom';
+
+function Search() {
+  return (
+    <div>
+        검색창
+    </div>
+  );
+}
+
+export default Search;
