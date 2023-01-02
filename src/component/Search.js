2ea67745ff57bae8a7a80e0a2d236bafbb696cab
kim-yeonsuu <yeonsu0329@kyonggi.ac.kr>
2023-01-02 11:55:24 +0900

    [design] 검색창 디자인 변경

--- src/component/Search.js
+++ src/component/Search.js
@@ -12,19 +12,18 @@ const Search = () => {
 
     return (
         <div>
-            <input type="text" value={search} onChange={onChange} placeholder="" />
+            <div className="input-group m-3">
+                <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="true">선택</button>
+                <ul className="dropdown-menu">
+                    <li><a className="dropdown-item" href="#">Action</a></li>
+                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                    <li><a className="dropdown-item" href="#">Separated link</a></li>
+                </ul>
+                <input type="text" className="form-control" aria-label="Text input with dropdown button" value={search} />
+                <button className="btn btn-outline-secondary" type="button">검색</button>
+            </div>
         </div>
-        // <div class="input-group mb-3">
-        //     <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</button>
-        //     <ul class="dropdown-menu">
-        //         <li><a class="dropdown-item" href="#">Action</a></li>
-        //         <li><a class="dropdown-item" href="#">Another action</a></li>
-        //         <li><a class="dropdown-item" href="#">Something else here</a></li>
-        //         <li><hr class="dropdown-divider"></li>
-        //         <li><a class="dropdown-item" href="#">Separated link</a></li>
-        //     </ul>
-        //     <input type="text" class="form-control" aria-label="Text input with dropdown button" value={search} onChange={onChange}>
-        // </div>
     );
 }
 
